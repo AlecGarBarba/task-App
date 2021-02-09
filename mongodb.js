713e865258851ca4748bc1e62ae339f77a002ac2
unknown <alecbarba@gmail.com>
2021-02-08 22:04:18 -0600

    CRUD full code

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,7 @@
 //create read update delete
-
+//to init mongo:
+//go to OneDrive/MongoDB-sever/bin and use this cmd:
+//.\mongod.exe --dbpath="C:/Users/alecb/OneDrive/MongoDB-data"
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const ObjectID = mongodb.ObjectID;
@@ -14,52 +16,46 @@ MongoClient.connect(connectionURL,{ useNewUrlParser: true, useUnifiedTopology: t
 
     const db =client.db(databaseName);
 
-    //This returns the first match
-   db.collection('users').findOne({ name: 'Danieeel', age: 2910 },(error, document)=>{
-    if(error){
-        return console.log("Unable to fetch specified user")
-    }
-    console.log(document)
-   })
-   //find by id (since it is stored in buffer, we need to objectiFy it)
-   db.collection('users').findOne({ _id: new ObjectID("601f694da9912e0b8c754d8d") },(error, document)=>{
-    if(error){
-        return console.log("Unable to fetch specified user")
-    }
-    console.log(document)
-   })
-
-   //find is diferent. returns a Cursor (apuntador) 
-
-   db.collection('users').find({age:24}).toArray((error,users)=>{
-    if(error){
-        return console.log("Unable to fetch specified user")
-    }
-    console.log(users)
-   })
-
-   db.collection('users').find({age:24}).count((error,count)=>{
-    if(error){
-        return console.log("Unable to fetch data")
-    }
-    console.log(count)
-   })
-
-   //FIND and findONE challenge
-   //fetch the last task by its id
-   //Find to fetch all tasks that are not completed
-   db.collection('tasks').findOne({ _id: new ObjectID("601f6cd0f31f0d1c4cb97a3a") },(error, document)=>{
-    if(error){
-        return console.log("Unable to fetch specified task")
-    }
-    console.log(document)
-   })
-
-   db.collection('tasks').find({ completed:false }).toArray((error,tasks)=>{
-    if(error){
-        return console.log("Unable to fetch data about tasks")
-    }
-    console.log(tasks)
-   })
-
+    /*db.collection('users').updateOne({
+        _id: new ObjectID('601f6a5f9d78343538ae1e46')
+    },{
+        $set:{
+            name: "Aleksonder"
+        }
+    }).then((result)=>{
+        console.log("Document updated!");
+        console.log(result)
+    }).catch((err)=>console.log("unable to update Aleksonder :( ", err))
+    
+   db.collection('users').updateOne({
+    _id: new ObjectID('601f6a5f9d78343538ae1e46')
+    },{
+        $inc:{
+            age: 1
+        }
+    }).then((result)=>{
+        console.log("Document updated!");
+        console.log(result)
+    }).catch((err)=>console.log("unable to update Aleksonder :( ", err))
+     
+   //update many to update all of the tasks. :)
+   db.collection('tasks').updateMany({
+       completed: true
+   },{
+       $set:{
+            completed: false
+       }
+   }).then((result)=>console.log(result.modifiedCount)).catch(()=>console.log("failed at the atempt"))
+    */ 
+   db.collection('tasks').deleteOne({
+       description: 'Clean the dishes'
+   }).then((result)=>{
+       console.log(result)
+   }).catch((error)=> console.log(error))
+   //remove a task :) challenge
+   db.collection('users').deleteOne({
+        name: 'Alec'
+    }).then((result)=>{
+        console.log(result.deletedCount)
+    }).catch((error)=> console.log(error))
 })
--- playground.js
+++ playground.js
@@ -117,4 +117,54 @@ MongoClient.connect(connectionURL,{ useNewUrlParser: true, useUnifiedTopology: t
     console.log(tasks)
    })
 
+
+   
+    //This returns the first match
+    db.collection('users').findOne({ name: 'Danieeel', age: 2910 },(error, document)=>{
+        if(error){
+            return console.log("Unable to fetch specified user")
+        }
+        console.log(document)
+       })
+       //find by id (since it is stored in buffer, we need to objectiFy it)
+       db.collection('users').findOne({ _id: new ObjectID("601f694da9912e0b8c754d8d") },(error, document)=>{
+        if(error){
+            return console.log("Unable to fetch specified user")
+        }
+        console.log(document)
+       })
+    
+       //find is diferent. returns a Cursor (apuntador) 
+    
+       db.collection('users').find({age:24}).toArray((error,users)=>{
+        if(error){
+            return console.log("Unable to fetch specified user")
+        }
+        console.log(users)
+       })
+    
+       db.collection('users').find({age:24}).count((error,count)=>{
+        if(error){
+            return console.log("Unable to fetch data")
+        }
+        console.log(count)
+       })
+    
+       //FIND and findONE challenge
+       //fetch the last task by its id
+       //Find to fetch all tasks that are not completed
+       db.collection('tasks').findOne({ _id: new ObjectID("601f6cd0f31f0d1c4cb97a3a") },(error, document)=>{
+        if(error){
+            return console.log("Unable to fetch specified task")
+        }
+        console.log(document)
+       })
+    
+       db.collection('tasks').find({ completed:false }).toArray((error,tasks)=>{
+        if(error){
+            return console.log("Unable to fetch data about tasks")
+        }
+        console.log(tasks)
+       })
+
 })
